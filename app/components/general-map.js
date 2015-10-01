f025daec10d9b987ed98141efb04aa6559d2ac17
Tony Navarro & Christoph Pirringer <student@epicodus.com>
2015-10-01 16:14:56 -0700

    display info-boxes on the google-map entry

--- app/components/general-map.js
+++ app/components/general-map.js
@@ -7,35 +7,48 @@ export default Ember.Component.extend({
     showMap(foodcarts) {
 
       var coordArray = [];
-      // debugger;
+      var contentArray = [];
+      var markerarray = [];
+
       foodcarts.forEach(function(foodcart) {
         coordArray.push([foodcart.get('latitude'), foodcart.get('longitude')]),
-        console.log(foodcart.get('latitude'))
+        contentArray.push([foodcart.get('name'), foodcart.get('website')])
+      });
+
+      var container = this.$('.map-display')[0];
+      var options = {
+        center: this.get('map').center(41.40338, 2.17403),
+        zoom: 5
+      };
+      var map = this.get('map').findMap(container, options);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker;
+      var i = 0;
+      var j = 0;
+
+      for ( i = 0; i < coordArray.length; i++) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(coordArray[i][0], coordArray[i][1]),
+          map: map,
+          title:coordArray[i][2]
+        });
+        markerarray.push(marker);
+      };
+
+      markerarray.forEach(function(marker, j){
+        google.maps.event.addListener(marker, 'click',  function() {
+          infowindow.setContent(contentArray[j-1][0] + " " + contentArray[j-1][1]);
+        console.log("j at setContent" + j);
+          infowindow.open(map, this);
+        });
+        console.log("j" + j);
+        console.log('content' + contentArray[j][0]);
+        j++;
       });
 
 
 
-      var container = this.$('.map-display')[0];
-      var options = {
-        center: this.get('map').center(51.503454, -0.119562),
-        zoom: 5
-      };
-      var map = this.get('map').findMap(container, options);
-      // var latLng2 = {lat: 51.503454, lng: -0.119562};
-      // var marker = new google.maps.Marker({
-      //   position: {lat: 51.503454, lng: -0.119562},
-      //   map: this.get('map').findMap(container, options),
-      //   title: 'Hello World!'
-      // });
-      var marker, i;
-      for ( i = 0; i < coordArray.length; i++) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(coordArray[i][0], coordArray[i][1]),
-          map: map
-        });
-          console.log(marker.position);
-      }
-      // this.get('map').findMap(container, marker);
     }
   }
 });
